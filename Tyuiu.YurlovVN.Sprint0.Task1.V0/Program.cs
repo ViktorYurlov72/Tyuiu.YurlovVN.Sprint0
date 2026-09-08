c2ac6a20f16b90936af0f86c9150bf3419cf18d2
VityaYurlov <vityaurlov530@gmail.com>
2026-09-09 04:21:06 +0500

    Task1

---
+++ Tyuiu.YurlovVN.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Tyuiu.SpirinIS.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello, World!");
+
+            
+            string name = "Игорь";
+            int age = 45;
+
+            
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет.");
+
+            
+            Console.ReadKey();
+        }
+    }
+}
